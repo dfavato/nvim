0f67e3ea4b9340ee72707d466c5464b2236ca510
Danilo Favato <dfavato@gmail.com>
2025-08-22 13:00:47 -0300

    remove treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,6 @@ return {
     build = ":TSUpdate",
     lazy = false,
     dependencies = {
-        "nvim-treesitter/nvim-treesitter-context",      -- Show context at top of window
         "nvim-treesitter/nvim-treesitter-textobjects",  -- Extra text objects
     },
 }
