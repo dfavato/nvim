f20f38e70f97ee8eb80c20b34a220c8519110b5f
Danilo Favato <dfavato@gmail.com>
2025-08-23 19:28:55 -0300

    opencode update

--- lua/plugins/opencode.lua
+++ lua/plugins/opencode.lua
@@ -4,15 +4,12 @@ return {
         -- Recommended for better prompt input, and required to use opencode.nvim's embedded terminal. Otherwise optional.
         { 'folke/snacks.nvim', opts = { input = { enabled = true } } },
     },
-    ---@type opencode.Opts
-    opts = {
-        agent = "task-helper",
-    },
     keys = {
         -- Recommended keymaps
         { '<leader>oA', function() require('opencode').ask() end,                              desc = 'Ask opencode', },
         { '<leader>oa', function() require('opencode').ask('@cursor: ') end,                   desc = 'Ask opencode about this',      mode = 'n', },
         { '<leader>oa', function() require('opencode').ask('@selection: ') end,                desc = 'Ask opencode about selection', mode = 'v', },
+        { '<leader>ob', function() require('opencode').ask('@buffers: ') end,                  desc = 'Ask opencode about buffers',   mode = 'n', },
         { '<leader>ot', function() require('opencode').toggle() end,                           desc = 'Toggle embedded opencode', },
         { '<leader>on', function() require('opencode').command('session_new') end,             desc = 'New session', },
         { '<leader>oy', function() require('opencode').command('messages_copy') end,           desc = 'Copy last message', },
