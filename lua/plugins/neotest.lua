8c9813ab6ddab7b5ea2c41fb9b20205f8250fe55
Danilo Favato <dfavato@gmail.com>
2025-08-23 19:28:31 -0300

    refactor neotest

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -8,5 +8,25 @@ return {
             "nvim-treesitter/nvim-treesitter",
             "nvim-neotest/neotest-python",
         },
+        keys = {
+            { "<F12>", "<cmd>Neotest summary<cr>", desc = "Neotest summary", noremap = true, silent = true },
+        },
+        opts = function()
+            return {
+                adapters = {
+                    require("neotest-python")({
+                        runner = "pytest",
+                        args = {
+                            "-q",
+                            "-vv",
+                            "--showlocals",
+                            "--disable-warnings",
+                            "--no-cov",
+                            "--no-header"
+                        },
+                    })
+                }
+            }
+        end,
     }
 }
