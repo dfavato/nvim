34652ba7d9dab1fb920110474f79c7a45a76ee57
Danilo Favato <dfavato@gmail.com>
2025-08-25 14:46:32 -0300

    improve treesitter config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,6 +4,42 @@ return {
     build = ":TSUpdate",
     lazy = false,
     dependencies = {
-        "nvim-treesitter/nvim-treesitter-textobjects",  -- Extra text objects
+        "nvim-treesitter/nvim-treesitter-textobjects", -- Extra text objects
     },
+    init = function()
+        config = require("nvim-treesitter.configs")
+        config.setup({
+            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python",
+                "javascript", "typescript", "sql", "html", "bash", "dockerfile", "fish",
+                "htmldjango", "json", "latex", "markdown", "yaml", "vue", "svelte" },
+
+            -- Install parsers synchronously (only applied to `ensure_installed`)
+            sync_install = false,
+
+            -- Automatically install missing parsers when entering buffer
+            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+            auto_install = true,
+
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+            indent = {
+                enable = true,
+            },
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+                    keymaps = {
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["ac"] = "@class.outer",
+                        ["ic"] = "@class.inner",
+                        ["as"] = "@statement.outer",
+                    },
+                },
+            },
+        })
+    end,
 }
