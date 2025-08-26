7a5103c27edce36f97d699e622ea7e0a34a61ba9
Danilo Favato <dfavato@gmail.com>
2025-08-26 11:15:59 -0300

    improve lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,21 +1,21 @@
-local function breadcrumbs ()
+local function breadcrumbs()
     return vim.fn["nvim_treesitter#statusline"]({
-        transform_fn = function (line, _node)
+        transform_fn = function(line, _node)
             return line:gsub('%s*[%[%(%{]*%s*$', ''):gsub('%(.-%)', ''):gsub(':', '')
         end
     })
 end
 return {
-    "nvim-lualine/lualine.nvim",  -- Status line
+    "nvim-lualine/lualine.nvim", -- Status line
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     lazy = false,
     opts = {
         sections = {
             lualine_a = {
-                { "mode", fmt = function (str) return str:sub(1, 1) end }
+                { "mode", fmt = function(str) return str:sub(1, 1) end }
             },
             lualine_b = {
-                { "branch", fmt = function (str) return str:sub(-30) end },
+                { "branch", fmt = function(str) return str:sub(-30) end },
             },
             lualine_c = {
                 "diff",
@@ -24,7 +24,7 @@ return {
                 breadcrumbs,
             },
             lualine_x = {
-                {"diagnostics", sources={"coc"}},
+                { "diagnostics", sources = { "coc" } },
                 "encoding",
                 "fileformat",
             },
