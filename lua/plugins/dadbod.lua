f247435ae3d181ca28401e7ef246c0c9dbbe7a9b
Danilo Favato <danilo.favato@worldquant.com>
2026-05-05 11:10:35 -0400

    dadbod helpers

--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -1,3 +1,36 @@
+local default_helpers = {
+    List = [[
+        SELECT *
+        FROM {table}
+        LIMIT 1
+    ]],
+    Count = [[
+        SELECT count(*)
+        FROM {table}
+    ]],
+}
+
+local vertica_helpers = vim.deepcopy(default_helpers)
+vertica_helpers['Columns'] = [[
+    SELECT *
+    FROM v_catalog.columns
+    WHERE table_name = '{table}'
+]]
+vertica_helpers['Size'] = [[
+    SELECT anchor_table_name, anchor_table_schema, sum(used_bytes) / (1024 ^ 3) as table_size_gb
+    FROM v_monitor.column_storage
+    WHERE anchor_table_name = '{table}'
+        AND anchor_table_schema = current_schema()
+    GROUP by anchor_table_name, anchor_table_schema
+]]
+
+local duckdb_helpers = vim.deepcopy(default_helpers)
+duckdb_helpers['Columns'] = [[
+    SELECT *
+    FROM information_schema.columns
+    WHERE table_name = '{table}'
+]]
+
 return {
     'kristijanhusak/vim-dadbod-ui',
     dependencies = {
@@ -14,5 +47,9 @@ return {
         -- Your DBUI configuration
         vim.g.db_ui_use_nerd_fonts = 1
         vim.g.db_ui_force_echo_notifications = 1
+        vim.g.db_ui_table_helpers = {
+            vertica = vertica_helpers,
+            duckdb2 = duckdb_helpers,
+        }
     end,
 }
